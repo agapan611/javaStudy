6bb811272a9a23021c6a6dc839aa104d726da3ae
agapan611 <kaitonosuke2232@gmail.com>
2021-02-28 20:53:05 +0900

    javalesson

---
+++ Study1.java
@@ -0,0 +1,18 @@
+package javaStudy;
+
+public class Study1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String name = "Kaito";
+		int age = 24;
+		System.out.print("私の名前は" + name + "です");
+		System.out.print("私の年齢は" + age + "です");
+		name = "Suzuki";
+		age = 18;
+		System.out.println();
+		System.out.print("私の名前は" + name + "です");
+		System.out.print("私の年齢は" + age + "です");
+	}
+
+}
---
+++ Study2.java
@@ -0,0 +1,13 @@
+package javaStudy;
+
+public class Study2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int number1 = 1000;
+		int number2 = 1500;
+		double ave = (number1 + number2) * 1.1;
+		System.out.println(ave);
+	}
+
+}
